c7de5b5c48a4244bbcbb6738fdb05f02ef971236
Alex Barry <alex.barry@gmail.com>
2020-03-14 20:11:25 -0400

    Use ROUTER_EVENT from constants in subscriptions

--- src/withRouter/subscriptions.js
+++ src/withRouter/subscriptions.js
@@ -1,12 +1,4 @@
-const ROUTER_EVENT = 'hyperapp-router-navigate';
-
-export const navigateByURI = (href) => {
-  const event = new CustomEvent(ROUTER_EVENT, {
-    detail: { href },
-  });
-
-  document.dispatchEvent(event);
-};
+import * as constants from './const';
 
 const makeFindRouteAndMatch = (routes) => (href) => {
   let route;
@@ -67,23 +59,23 @@ const PushFX = (dispatch, props) => {
     const result = findRouteAndMatch(event.detail.href);
     return onPush(result.route, result.match);
   };
-  document.addEventListener(ROUTER_EVENT, onNavigate);
+  document.addEventListener(constants.ROUTER_EVENT, onNavigate);
 
-  const onPop = () => {
-    const result = findRouteAndMatch(window.location.pathname);
-    if (result) setCurrentRoute(result.route, result.match);
+  const onPop = (event) => {
+    const result = findRouteAndMatch(event.originalTarget.location.pathname);
+    return setCurrentRoute(result.route, result.match);
   };
   window.addEventListener('popstate', onPop);
 
   const init = () => {
-    return onPop();
+    return onPop({ originalTarget: window });
   };
 
   setTimeout(init, 0);
 
   return () => {
     document.removeEventListener('click', onClick);
-    document.removeEventListener(ROUTER_EVENT, onNavigate);
+    document.removeEventListener(constants.ROUTER_EVENT, onNavigate);
     window.removeEventListener('popstate', onPop);
   };
 };
