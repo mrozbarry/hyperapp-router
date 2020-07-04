03c84c6adf7e045b5f4617aa69707a6b063fa9a7
Alex Barry <alex.barry@gmail.com>
2020-07-03 21:05:32 -0400

    Fix baseUrl merging

--- src/withRouter/index.js
+++ src/withRouter/index.js
@@ -3,14 +3,14 @@ import { match } from 'path-to-regexp';
 import * as subscriptions from './subscriptions';
 
 const ensureStartsWithSlash = part => (
-  `/${part.replace(/^\//, '').replace(/\/+$/, '')}`
+  `/${part.replace(/^\/+/, '')}`
 );
 
 const routesToArray = ({ routes: routesObject, baseUrl }) => Object.keys(routesObject)
   .reduce((routes, initialPath) => {
     const path = ensureStartsWithSlash([
-      (baseUrl || '/').replace(/.+(\/$)/, ''),
-      initialPath.replace(/^\//, ''),
+      baseUrl.replace(/.+(\/$)/, ''),
+      initialPath.replace(/^\/+/, ''),
     ]
       .map(ensureStartsWithSlash)
       .join(''));
@@ -32,7 +32,10 @@ export default (app) => ({ router, ...rest }) => {
     ? rest.subscriptions
     : () => [];
 
-  const routes = routesToArray(router);
+  const routes = routesToArray({
+    baseUrl: '',
+    ...router,
+  });
 
   return app({
     ...rest,
