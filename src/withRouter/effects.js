b8d2d28eaa93178434c357b1d1dfbb0ae4fd74ca
Bruno Dias <dias.h.bruno@gmail.com>
2021-02-11 22:09:48 -0300

    fixed(navigation): propagate options to navigate function.

--- src/withRouter/effects.js
+++ src/withRouter/effects.js
@@ -1,7 +1,7 @@
 import { navigate } from './lib/navigate';
 
-const NavigateFX = (_dispatch, href) => {
-  navigate(href);
+const NavigateFX = (_dispatch, { href, options }) => {
+  navigate(href, options);
 };
 export const Navigate = props => [
   NavigateFX,
